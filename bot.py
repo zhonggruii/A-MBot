21b317ac553a631eb0460428d8883ad06a215146
zhonggruii <leezhongrui1@gmail.com>
2025-08-23 10:27:47 +0800

    Edit rules & format bot forwarding message * No profanities * No spamming

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "1. Please make a reasonable challenge i.e nothing dangerous or illegal\n"
         "2. Please issue a challenge based on your mortal's tolerance level\n"
         "3. Please be respectful and dont use profanities\n"
-        "4. Please dont spam the bot"
+        "4. Please dont spam the bot\n"
         "5. Please indicate your mortal @ at the start of the message\n"
         "i.e @zhonggruii Please bark 3 times\n"
         "If you have doubts about whether your challenge is okay, ask your RA or HH\n\n"
